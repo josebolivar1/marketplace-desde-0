cb2ea44e0ebbac7badab528ae8f5636568a0a42b
Jose Bolivar <josevalle.info@gmail.com>
2016-09-08 13:57:50 +0000

    7.6 GRUPO. Completamos acción list

--- src/AppBundle/Controller/PublicController.php
+++ src/AppBundle/Controller/PublicController.php
@@ -20,6 +20,39 @@ class PublicController extends Controller
     */
     public function listAction(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+        $repositorioTrayecto = $em->getRepository("AppBundle:Trayecto");
+        $trayectos = $repositorioTrayecto->findAll();
         
+         // Si no se indica un filtro para la fecha, se muestran todos los trayectos
+        if ($request->get('posted') != "" || $request->get('country') != "") {
+            $parameters=array();
+            $queryBuilder = $repositorioTrayecto->createQueryBuilder('trayecto');
+            if ($request->get('country')!="") {
+                $queryBuilder->where('trayecto.origen = :country')->orWhere('trayecto.destino = :country');
+                $parameters['country']=$request->get('country');
+            }   
+            if($request->get('posted') != ""){
+                if ($request->get('country')!="") {
+                    $queryBuilder->andWhere('fecha.createdAt > :posted');
+                } 
+                else {
+                    $queryBuilder->where('fecha.createdAt > :posted');
+                } 
+                $date=new DateTime();
+                $date->modify('-'.$request->get('posted').' day');
+                $parameters['posted']=$date;
+            }
+            $trayectosFiltrados =$queryBuilder->setParameters($parameters)
+                ->getQuery()->execute();
+        } else {
+            $trayectosFiltrados = $trayectos;
+        }    
+    
+        return $this->render('list/index.html.twig', array(
+            'trayectos' => $trayectosFiltrados,
+            'country' => $request->get('country'),
+            'posted' => $request->get('posted'),
+        ));
     }
 }
